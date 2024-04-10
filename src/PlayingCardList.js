1e68f3502a6842b5d0f9e823a2f57824de03878e
Marissa <mickles.marissa@gmail.com>
2024-04-10 18:21:14 -0400

    41.10 React Cards exercise completed steps 1-4; submitting before finishing Further Study per exercise instructions

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAxios } from "./hooks";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
@@ -6,13 +6,14 @@ import "./PlayingCardList.css";
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+  const baseURL = "https://deckofcardsapi.com/api/deck/new/draw/";
+  const [cards, addCard] = useAxios(baseURL);
   
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+      <button onClick={() => addCard()}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid'; 
+import React from "react";
 import { useAxios } from "./hooks";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
